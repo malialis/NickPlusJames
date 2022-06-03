2db09addb02389ba29ced96dba7784e5435173af
malialis <batnick@gmail.com>
2022-06-03 12:04:26 -0600

    Update GameManager.cs

--- Assets/Scripts/Bard/Managers/GameManager.cs
+++ Assets/Scripts/Bard/Managers/GameManager.cs
@@ -3,11 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class PlayerStats : MonoBehaviour
 {
-    
+    [SerializeField] private float maxHealth;
 
+    [SerializeField] private GameObject deathChunkParticles;
+    [SerializeField] private GameObject deathBloodParticles;
 
-    
+    private float currentHealth;
+
+    private GameManager gm;
+
+    private void Start() 
+    {
+        currentHealth = maxHealth;  
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();      
+    }
+
+    public void DecreaseHealth(float amount)
+    {
+        currentHealth -= amount;
+
+        if(currentHealth <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    private voi Die()
+    {
+        Instantiate(deathChunkParticles, transform.position, deathChunkParticles.transform.rotation);
+        Instantiate(deathBloodParticles, transform.position, deathBloodParticles.transform.rotation);
+        gm.respawn();
+        Destroy(gameObject, 0.5f);
+    }
 }
 
